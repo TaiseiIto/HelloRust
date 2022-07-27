19f68c79a46c9ae992c3c81a1a87b36fc1f85ad6
TaiseiIto <taisei1212@outlook.jp>
2022-07-27 17:32:43 +0900

    guessing_game

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -3,10 +3,8 @@ use std::io::Write;
 
 fn main() {
     let secret_number:u8 = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is : {}", secret_number);
     loop {
         let mut guess:String = String::new();
-        println!("Guess the number!");
         print!("Please input your guess : ");
         std::io::stdout().flush().unwrap();
         std::io::stdin().read_line(&mut guess).expect("Failed to read line");
