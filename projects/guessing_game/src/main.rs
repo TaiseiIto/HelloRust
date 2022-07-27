6c9b06ff70b80e7f16f7c7e54010e6227adc880b
TaiseiIto <taisei1212@outlook.jp>
2022-07-27 16:41:19 +0900

    guessing_game

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,3 +1,11 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut guess:String = String::new();
+    println!("Guess the number!");
+    print!("Please input your guess : ");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut guess).expect("Failed to read line");
+    println!("You guessed : {}", guess);
 }
+
