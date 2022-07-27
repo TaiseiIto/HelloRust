ecb2348e6357f26d6ea007341bb7fbc423f11c55
TaiseiIto <taisei1212@outlook.jp>
2022-07-27 17:19:51 +0900

    guess_game

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -5,16 +5,21 @@ fn main() {
     let mut guess:String = String::new();
     let secret_number:u8 = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is : {}", secret_number);
-    println!("Guess the number!");
-    print!("Please input your guess : ");
-    std::io::stdout().flush().unwrap();
-    std::io::stdin().read_line(&mut guess).expect("Failed to read line");
-    let guess:u8 = guess.trim().parse().expect("Please type a number!");
-    println!("You guessed : {}", guess);
-    match guess.cmp(&secret_number) {
-        std::cmp::Ordering::Less => println!("Too small!"),
-        std::cmp::Ordering::Greater => println!("Too big!"),
-        std::cmp::Ordering::Equal => println!("You win!"),
+    loop {
+        println!("Guess the number!");
+        print!("Please input your guess : ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess:u8 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed : {}", guess);
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too small!"),
+            std::cmp::Ordering::Greater => println!("Too big!"),
+            std::cmp::Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
 
