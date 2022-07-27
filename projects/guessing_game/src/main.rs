718a151cdd9a7fe259f54be196d7d21efd498cee
TaiseiIto <taisei1212@outlook.jp>
2022-07-27 17:25:07 +0900

    guessing_game

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -10,7 +10,13 @@ fn main() {
         print!("Please input your guess : ");
         std::io::stdout().flush().unwrap();
         std::io::stdin().read_line(&mut guess).expect("Failed to read line");
-        let guess:u8 = guess.trim().parse().expect("Please type a number!");
+        let guess:u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
         println!("You guessed : {}", guess);
         match guess.cmp(&secret_number) {
             std::cmp::Ordering::Less => println!("Too small!"),
