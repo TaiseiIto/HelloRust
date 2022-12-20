5eae59e2eeaca188d8f851f90961a423edc721ff
TaiseiIto <taisei1212@outlook.jp>
2022-12-20 21:53:23 +0900

    4.3

--- projects/possession/src/main.rs
+++ projects/possession/src/main.rs
@@ -6,6 +6,38 @@ fn change(some_string: &mut String) {
     some_string.push_str(", world!");
 }
 
+fn first_word(s: &String) -> &str {
+	let mut begin_index: usize = 0;
+	let mut begin_index_found: bool = false;
+	let mut end_index: usize = s.len();
+	let mut end_index_found: bool = false;
+	for (i, &byte) in s.as_bytes().iter().enumerate() {
+		match (begin_index_found, end_index_found, byte == b' ') {
+			(false, false, false) => {
+				begin_index = i;
+				begin_index_found = true;
+			},
+			(false, false, true ) => {},
+			(false, true , false) => {},
+			(false, true , true ) => {},
+			(true , false, false) => {},
+			(true , false, true ) => {
+				end_index = i;
+				end_index_found = true;
+				break;
+			},
+			(true , true , false) => {},
+			(true , true , true ) => {},
+		}
+	}
+	match (begin_index_found, end_index_found) {
+		(false, false) => &s[..0],
+		(false, true ) => &s[..0],
+		(true , false) => &s[begin_index..],
+		(true , true ) => &s[begin_index..end_index],
+	}
+}
+
 fn gives_ownership() -> String {
     String::from("hello")
 }
@@ -38,10 +70,52 @@ fn main() {
     let r2: &mut String = &mut s;
     change(r2);
     println!("s = {}", s);
+	let s: String = String::from("  hello,  world!  hey hoo!");
+	println!("The first word is {}.", first_word(&s));
+	println!("The second word is {}.", second_word(&s));
 }
 
 fn makes_copy(some_integer: i32) {
     println!("some_integer = {}", some_integer);
+}
+
+fn second_word(s: &String) -> &str {
+	let mut word_counter: u32 = 0;
+	let mut begin_index: usize = 0;
+	let mut begin_index_found: bool = false;
+	let mut end_index: usize = s.len();
+	let mut end_index_found: bool = false;
+	for (i, &byte) in s.as_bytes().iter().enumerate() {
+		match (begin_index_found, end_index_found, byte == b' ') {
+			(false, false, false) => {
+				begin_index = i;
+				begin_index_found = true;
+			},
+			(false, false, true ) => {},
+			(false, true , false) => {},
+			(false, true , true ) => {},
+			(true , false, false) => {},
+			(true , false, true ) => {
+				end_index = i;
+				end_index_found = true;
+				if word_counter == 1 {
+					break;
+				} else {
+					word_counter += 1;
+					begin_index_found = false;
+					end_index_found = false;
+				}
+			},
+			(true , true , false) => {},
+			(true , true , true ) => {},
+		}
+	}
+	match (begin_index_found, end_index_found) {
+		(false, false) => &s[..0],
+		(false, true ) => &s[..0],
+		(true , false) => &s[begin_index..],
+		(true , true ) => &s[begin_index..end_index],
+	}
 }
 
 fn takes_and_gives_back(a_string: String) -> String {
