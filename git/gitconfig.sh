232a63137942daa2ae4141e3f413416396a70d5e
TaiseiIto <taisei1212@outlook.jp>
2022-12-20 14:39:25 +0900

    gitconfig

--- git/gitconfig.sh
+++ git/gitconfig.sh
@@ -7,13 +7,28 @@
 
 currentdir=$(pwd)
 cd $(dirname $0)
-echo -n "Your GitHub email:"
-read email
-echo -n "Your GitHub name:"
-read name
+read -p "Your GitHub email:" email
+read -p "Your GitHub name:" name
+unset password
+prompt="Password of ${email}:"
+while true ; do
+	read -n 1 -p "$prompt" -r -s char
+	if [[ $char == $'\0' ]]; then
+		break
+	fi
+	prompt="*"
+	password+="$char"
+done
+echo
+git config --global sendemail.confirm auto
+git config --global sendemail.smtpserver smtp.office365.com
+git config --global sendemail.smtpencryption tls
+git config --global sendemail.smtpuser $email
+git config --global sendemail.smtpserverport 587
+git config --global sendemail.smtppass $password
 git config --global user.email $email
 git config --global user.name $name
-git config --global user.signingkey 5F1ABB3B591CCA48
+git config --global user.signingkey $(head -n1 /root/.gnupg/signingkey.txt)
 git config --global --add commit.gpgsign true
 git remote set-url origin git@github.com:TaiseiIto/HelloRust.git
 cat gitconfig >> /root/.gitconfig
