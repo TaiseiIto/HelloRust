e7a0049ec00171195a83472a7c548c2fc39d2214
TaiseiIto <taisei1212@outlook.jp>
2022-12-21 21:30:07 +0900

    7.3

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -10,6 +10,37 @@ mod front_of_house {
 	}
 }
 
+fn serve_order() {}
+
+mod back_of_house {
+	#[derive(Debug)]
+	pub struct Breakfast {
+		pub toast: String,
+		seasonal_fruit: String,
+	}
+	impl Breakfast {
+		pub fn summer(toast: &str) -> Breakfast {
+			Breakfast {
+				toast: String::from(toast),
+				seasonal_fruit: String::from("peaches"),
+			}
+		}
+		pub fn get_seasonal_fruit(&self) -> &String {
+			&self.seasonal_fruit
+		}
+	}
+	#[derive(Debug)]
+	pub enum Appetizer {
+		Soup,
+		Salad,
+	}
+	pub fn fix_incorrect_order() {
+		cook_order();
+		super::serve_order();
+	}
+	fn cook_order() {}
+}
+
 pub fn eat_at_restaurant() {
 	// Absolute path
 	crate::front_of_house::hosting::add_to_waitlist();
@@ -18,5 +49,16 @@ pub fn eat_at_restaurant() {
 	front_of_house::serving::take_order();
 	front_of_house::serving::serve_order();
 	front_of_house::serving::take_payment();
+	back_of_house::fix_incorrect_order();
+	let mut meal = back_of_house::Breakfast::summer("Rye");
+	println!("meal = {:#?}", meal);
+	meal.toast = String::from("Wheat");
+	println!("I'd like {} toast please.", meal.toast);
+	println!("meal = {:#?}", meal);
+	println!("meal.get_seasonal_fruit() = {}", meal.get_seasonal_fruit());
+	let order1 = back_of_house::Appetizer::Soup;
+	let order2 = back_of_house::Appetizer::Salad;
+	println!("order1 = {:#?}", order1);
+	println!("order2 = {:#?}", order2);
 }
 
