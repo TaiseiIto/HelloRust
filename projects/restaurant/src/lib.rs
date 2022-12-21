01dfd6b7a982c45bfcc15ca9358b4e390e2e8662
TaiseiIto <taisei1212@outlook.jp>
2022-12-21 20:50:48 +0900

    7.3

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod front_of_house {
+	pub mod hosting {
+		pub fn add_to_waitlist() {}
+		pub fn seat_at_table() {}
+	}
+	pub mod serving {
+		pub fn take_order() {}
+		pub fn serve_order() {}
+		pub fn take_payment() {}
+	}
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn eat_at_restaurant() {
+	// Absolute path
+	crate::front_of_house::hosting::add_to_waitlist();
+	crate::front_of_house::hosting::seat_at_table();
+	// Relative path
+	front_of_house::serving::take_order();
+	front_of_house::serving::serve_order();
+	front_of_house::serving::take_payment();
 }
+
