5fa2818d54e8a7d416085458e9bbbe475e157307
TaiseiIto <taisei1212@outlook.jp>
2022-12-23 15:12:43 +0900

    8.3.1

--- projects/practice_8_3_1/src/main.rs
+++ projects/practice_8_3_1/src/main.rs
@@ -14,14 +14,31 @@ fn median(sequence: &Vec<i8>) -> i8 {
 	sorted[sorted.len() / 2]
 }
 
+fn mode(sequence: &Vec<i8>) -> i8 {
+	let mut value2count: std::collections::HashMap<i8, u32> = std::collections::HashMap::new();
+	for n in sequence {
+		*value2count.entry(*n).or_insert(0) += 1;
+	}
+	let mut max_count: u32 = 0;
+	let mut mode: i8 = 0;
+	for (value, count) in value2count {
+		if max_count < count {
+			max_count = count;
+			mode = value;
+		}
+	}
+	mode
+}
+
 fn main() {
 	let mut rng = rand::thread_rng();
 	let mut sequence: Vec<i8> = Vec::new();
-	for _ in 0..10 {
+	for _ in 0..100 {
 		sequence.push(rng.gen());
 	}
 	println!("sequence = {:?}", sequence);
 	println!("mean(sequence) = {:?}", mean(&sequence));
 	println!("median(sequence) = {:?}", median(&sequence));
+	println!("mode(sequence) = {:?}", mode(&sequence));
 }
 
