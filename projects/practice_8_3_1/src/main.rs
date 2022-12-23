feeef4d37fceb3be57f8923e7d3fb84ff49960c1
TaiseiIto <taisei1212@outlook.jp>
2022-12-23 14:27:50 +0900

    8.3.1

--- projects/practice_8_3_1/src/main.rs
+++ projects/practice_8_3_1/src/main.rs
@@ -1,8 +1,20 @@
 use rand::Rng;
 
+fn mean(sequence: &Vec<i8>) -> f64 {
+	let mut sum: i32 = 0;
+	for n in sequence {
+		sum += *n as i32;
+	}
+	sum as f64 / sequence.len() as f64
+}
+
 fn main() {
 	let mut rng = rand::thread_rng();
-	let x: u8 = rng.gen();
-	println!("x = {}", x);
+	let mut sequence: Vec<i8> = Vec::new();
+	for _ in 0..10 {
+		sequence.push(rng.gen());
+	}
+	println!("sequence = {:?}", sequence);
+	println!("mean(sequence) = {:?}", mean(&sequence));
 }
 
