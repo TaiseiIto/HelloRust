3e1faf77f881ebd4560941994dc4f22b74d69f95
TaiseiIto <taisei1212@outlook.jp>
2022-12-23 20:50:01 +0900

    8.3.3

--- projects/practice_8_3_3/src/main.rs
+++ projects/practice_8_3_3/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Write;
+
+#[derive(Debug)]
+enum Command {
+	Quit,
 }
+
+fn analyse_command(line: &str) -> Option<Command> {
+	let mut words: std::str::SplitWhitespace = line.split_whitespace();
+	match words.next() {
+		Some(command) => match &*command.to_lowercase() {
+			"quit" => Some(Command::Quit),
+			_ => None,
+		},
+		None => None,
+	}
+}
+
+fn main() {
+	let mut command: String = String::new();
+	print!("manage employees > ");
+	std::io::stdout().flush().unwrap();
+	std::io::stdin().read_line(&mut command).expect("Failed to read line");
+	let command: Option<Command> = analyse_command(&command[..]);
+	println!("command = {:?}", command);
+}
+
