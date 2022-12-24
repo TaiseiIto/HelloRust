a21dd932095f030108fbc9add5af944d3b2258c4
TaiseiIto <taisei1212@outlook.jp>
2022-12-24 14:36:29 +0900

    9.2

--- projects/error/src/main.rs
+++ projects/error/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+	let file_name: &str = "hello.txt";
+	let file: std::fs::File = match std::fs::File::open(file_name) {
+		Ok(file) => file,
+		Err(ref error) if error.kind() == std::io::ErrorKind::NotFound => match std::fs::File::create(file_name) {
+			Ok(file) => file,
+			Err(error) => panic!("Tried to create file but there was a problem: {:?}", error),
+		}
+		Err(error) => panic!("There was a problem opening the file: {:?}", error),
+	};
 }
+
