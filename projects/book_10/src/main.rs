a2574b73cad265492caf8f751c824797ba564f32
TaiseiIto <taisei1212@outlook.jp>
2022-12-24 16:18:34 +0900

    10

--- projects/book_10/src/main.rs
+++ projects/book_10/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+	let numbers: Vec<i32> = vec![34, 50, 25, 100, 65];
+	let mut largest: i32 = numbers[0];
+	for number in numbers {
+		if largest < number {
+			largest = number;
+		}
+	}
+	println!("The largest number is {}.", largest);
 }
+
