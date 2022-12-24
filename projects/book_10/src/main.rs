ee90ee7fd82c860713d0198d5ddd230b9c3f2809
TaiseiIto <taisei1212@outlook.jp>
2022-12-24 16:50:23 +0900

    10

--- projects/book_10/src/main.rs
+++ projects/book_10/src/main.rs
@@ -1,10 +1,10 @@
-fn largest(numbers: Vec<i32>) -> Result<i32, ()> {
+fn largest(numbers: &Vec<i32>) -> Result<i32, ()> {
 	match numbers.get(0) {
 		Some(number) => {
 			let mut largest: i32 = *number;
 			for number in numbers {
-				if largest < number {
-					largest = number;
+				if largest < *number {
+					largest = *number;
 				}
 			}
 			Ok(largest)
@@ -15,10 +15,10 @@ fn largest(numbers: Vec<i32>) -> Result<i32, ()> {
 
 fn main() {
 	let numbers: Vec<i32> = vec![34, 50, 25, 100, 65];
-	let largest_number: i32 = largest(numbers).expect("There is no largest number.");
+	let largest_number: i32 = largest(&numbers).expect("There is no largest number.");
 	println!("The largest number is {}.", largest_number);
 	let numbers: Vec<i32> = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-	let largest_number: i32 = largest(numbers).expect("There is no largest number.");
+	let largest_number: i32 = largest(&numbers).expect("There is no largest number.");
 	println!("The largest number is {}.", largest_number);
 }
 
