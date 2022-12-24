d57f093e9b20b3b10a9fdc21b002d9f4cab5bc7d
TaiseiIto <taisei1212@outlook.jp>
2022-12-24 21:53:27 +0900

    10

--- projects/book_10/src/main.rs
+++ projects/book_10/src/main.rs
@@ -1,10 +1,10 @@
-fn largest<T: PartialOrd + Copy>(numbers: &Vec<T>) -> Result<T, ()> {
+fn largest<T: PartialOrd>(numbers: &Vec<T>) -> Result<&T, ()> {
 	match numbers.get(0) {
 		Some(number) => {
-			let mut largest: T = *number;
+			let mut largest: &T = number;
 			for number in numbers {
-				if largest < *number {
-					largest = *number;
+				if *largest < *number {
+					largest = number;
 				}
 			}
 			Ok(largest)
@@ -15,10 +15,10 @@ fn largest<T: PartialOrd + Copy>(numbers: &Vec<T>) -> Result<T, ()> {
 
 fn main() {
 	let numbers: Vec<i32> = vec![34, 50, 25, 100, 65];
-	let largest_number: i32 = largest(&numbers).expect("There is no largest number.");
+	let largest_number: &i32 = largest(&numbers).expect("There is no largest number.");
 	println!("The largest number is {}.", largest_number);
 	let numbers: Vec<i32> = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-	let largest_number: i32 = largest(&numbers).expect("There is no largest number.");
+	let largest_number: &i32 = largest(&numbers).expect("There is no largest number.");
 	println!("The largest number is {}.", largest_number);
 }
 
