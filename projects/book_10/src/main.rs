62d93c4ce02262271c7c308ab70fdb0d3740c74a
TaiseiIto <taisei1212@outlook.jp>
2022-12-24 16:39:34 +0900

    10

--- projects/book_10/src/main.rs
+++ projects/book_10/src/main.rs
@@ -1,11 +1,21 @@
+fn largest(numbers: Vec<i32>) -> Result<i32, ()> {
+	match numbers.get(0) {
+		Some(number) => {
+			let mut largest: i32 = *number;
+			for number in numbers {
+				if largest < number {
+					largest = number;
+				}
+			}
+			Ok(largest)
+		},
+		None => Err(()),
+	}
+}
+
 fn main() {
 	let numbers: Vec<i32> = vec![34, 50, 25, 100, 65];
-	let mut largest: i32 = numbers[0];
-	for number in numbers {
-		if largest < number {
-			largest = number;
-		}
-	}
+	let largest: i32 = largest(numbers).expect("There is no largest number.");
 	println!("The largest number is {}.", largest);
 }
 
