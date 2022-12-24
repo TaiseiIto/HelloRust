56a1fc3d6c9079b53563841a898ed1258b4aee7a
TaiseiIto <taisei1212@outlook.jp>
2022-12-24 15:01:29 +0900

    9.2

--- projects/error/src/main.rs
+++ projects/error/src/main.rs
@@ -1,6 +1,21 @@
+use std::io::Read;
+
+fn read_username_from_file() -> Result<String, std::io::Error> {
+	let file_name: &str = "hello.txt";
+	let mut file: std::fs::File = match std::fs::File::open(file_name) {
+		Ok(file) => file,
+		Err(error) => return Err(error),
+	};
+	let mut username: String = String::new();
+	match file.read_to_string(&mut username) {
+		Ok(_) => Ok(username),
+		Err(error) => Err(error),
+	}
+}
+
 fn main() {
 	let file_name: &str = "hello.txt";
-	let file: std::fs::File = match std::fs::File::open(file_name) {
+	match std::fs::File::open(file_name) {
 		Ok(file) => file,
 		Err(ref error) if error.kind() == std::io::ErrorKind::NotFound => match std::fs::File::create(file_name) {
 			Ok(file) => file,
@@ -8,5 +23,9 @@ fn main() {
 		}
 		Err(error) => panic!("There was a problem opening the file: {:?}", error),
 	};
+	match read_username_from_file() {
+		Ok(username) => println!("username: {}", username),
+		Err(error) => panic!("Can't read user name: {:?}", error),
+	}
 }
 
