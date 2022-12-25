a48a69fee435cb183c1fd5573e368bcfbb5fde53
TaiseiIto <taisei1212@outlook.jp>
2022-12-25 20:41:49 +0900

    12.1

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+	let mut args: std::env::Args = std::env::args();
+	let program_name: String = args.next().expect("There is no program name.");
+	let query: String = args.next().expect("There is no query.");
+	let file_name: String = args.next().expect("There is no file name.");
+	println!("program_name = \"{}\"", program_name);
+	println!("query = \"{}\"", query);
+	println!("file_name = \"{}\"", file_name);
 }
+
