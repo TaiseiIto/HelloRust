ac72eac6a1566f34949f4bfac02b6d1c33a71c12
TaiseiIto <taisei1212@outlook.jp>
2022-12-25 19:50:15 +0900

    11.3

--- projects/book_11_3/src/lib.rs
+++ projects/book_11_3/src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+fn internal_adder(a: i32, b: i32) -> i32 {
+	a + b
+}
+
+pub fn add_two(a: i32) -> i32 {
+	internal_adder(a, 2)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+	#[test]
+	fn test_internal_adder() {
+		assert_eq!(internal_adder(2, 2), 4);
+	}
+
+	#[test]
+	fn test_add_two() {
+		assert_eq!(add_two(2), 4);
+	}
 }
+
