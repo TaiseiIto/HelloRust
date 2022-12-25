dca743e3238c2ad8fbc349c107b85fd352204581
TaiseiIto <taisei1212@outlook.jp>
2022-12-25 16:38:52 +0900

    11.1

--- projects/book_11_1/src/lib.rs
+++ projects/book_11_1/src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug)]
+pub struct Rectangle {
+	width: u32,
+	height: u32,
+}
+
+impl Rectangle {
+	pub fn can_hold(&self, other: &Rectangle) -> bool {
+		other.width < self.width && other.height < self.height
+	}
+
+	pub fn get_width(&self) -> &u32 {
+		&self.width
+	}
+
+	pub fn get_height(&self) -> &u32 {
+		&self.height
+	}
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+	const LARGER: Rectangle = Rectangle {
+		width: 8,
+		height: 7,
+	};
+	const SMALLER: Rectangle = Rectangle {
+		width: 5,
+		height: 1,
+	};
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn larger_can_hold_smaller() {
+		assert!(LARGER.can_hold(&SMALLER));
     }
+
+	#[test]
+	fn smaller_cannot_hold_larger() {
+		assert!(!SMALLER.can_hold(&LARGER));
+	}
 }
+
