0dc338a4869d9b685e4dfae05ce4b0d393f127bc
TaiseiIto <taisei1212@outlook.jp>
2022-12-25 14:57:29 +0900

    10.3

--- projects/book_10_3/src/main.rs
+++ projects/book_10_3/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+	part: &'a str,
 }
+
+fn longest<'a>(str0: &'a str, str1: &'a str) -> &'a str {
+	if str0.len() < str1.len() {
+		str1
+	} else {
+		str0
+	}
+}
+
+fn main() {
+	println!("The longest string is \"{}\"", longest("abcd", "xyz"));
+	let novel = "Call me Ishmael. Some years ago ...".to_string();
+	let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+	let important_excerpt = ImportantExcerpt {
+		part: first_sentence,
+	};
+	println!("Important excerpt = {:?}", important_excerpt);
+}
+
