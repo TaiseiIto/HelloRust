bb7e8a8ddf5566825e0425c48680b271af69cb9d
TaiseiIto <taisei1212@outlook.jp>
2022-12-25 22:13:31 +0900

    12.3

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -14,18 +14,26 @@ impl Config {
 	}
 }
 
+fn run(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
+	println!("query = \"{}\"", config.query);
+	println!("file_name = \"{}\"", config.file_name);
+	let file_name: &str = &config.file_name;
+	let mut file: std::fs::File = std::fs::File::open(&file_name)?;
+	let mut contents: String = String::new();
+	file.read_to_string(&mut contents)?;
+	println!("Contents of \"{}\"", file_name);
+	println!("{}", contents);
+	Ok(())
+}
+
 fn main() {
 	let config: Config = Config::new(&mut std::env::args()).unwrap_or_else(|error| {
 		println!("Error while parsing arguments: {}", error);
 		std::process::exit(1);
 	});
-	println!("query = \"{}\"", config.query);
-	println!("file_name = \"{}\"", config.file_name);
-	let file_name: &str = &config.file_name;
-	let mut file: std::fs::File = std::fs::File::open(&file_name).expect(&format!("\"{}\" is not found.", file_name));
-	let mut contents: String = String::new();
-	file.read_to_string(&mut contents).expect(&format!("Something went wrong reading \"{}\"", file_name));
-	println!("Contents of \"{}\"", file_name);
-	println!("{}", contents);
+	if let Err(error) = run(&config) {
+		println!("Error while running: {}", error);
+		std::process::exit(2);
+	}
 }
 
