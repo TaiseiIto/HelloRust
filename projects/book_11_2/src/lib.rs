62a16dd9fd0177220934bce27c74a0a1538f21ba
TaiseiIto <taisei1212@outlook.jp>
2022-12-25 19:10:22 +0900

    11.2

--- projects/book_11_2/src/lib.rs
+++ projects/book_11_2/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn print_and_return_10(a: i32) -> i32 {
+	println!("I got the value {}.", a);
+	10
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+	#[test]
+	fn this_test_will_pass() {
+		assert_eq!(print_and_return_10(1), 10);
+	}
+
+	#[test]
+	fn this_test_will_fail() {
+		assert_eq!(print_and_return_10(0), 0);
+	}
 }
+
