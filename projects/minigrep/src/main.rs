57bb372aa5aa91edd877c139da12e425427820a7
TaiseiIto <taisei1212@outlook.jp>
2022-12-25 20:53:01 +0900

    12.2

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::prelude::*;
+
 fn main() {
 	let mut args: std::env::Args = std::env::args();
 	let program_name: String = args.next().expect("There is no program name.");
@@ -6,5 +8,10 @@ fn main() {
 	println!("program_name = \"{}\"", program_name);
 	println!("query = \"{}\"", query);
 	println!("file_name = \"{}\"", file_name);
+	let mut file: std::fs::File = std::fs::File::open(&file_name).expect(&format!("\"{}\" is not found.", &file_name));
+	let mut contents: String = String::new();
+	file.read_to_string(&mut contents).expect(&format!("Something went wrong reading \"{}\"", &file_name));
+	println!("Contents of \"{}\"", &file_name);
+	println!("{}", contents);
 }
 
