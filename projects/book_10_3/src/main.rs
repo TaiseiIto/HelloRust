22f16ccf55948770bd6ece5963f1d56afa0d048a
TaiseiIto <taisei1212@outlook.jp>
2022-12-25 15:40:41 +0900

    10.3

--- projects/book_10_3/src/main.rs
+++ projects/book_10_3/src/main.rs
@@ -3,6 +3,12 @@ struct ImportantExcerpt<'a> {
 	part: &'a str,
 }
 
+impl<'a> ImportantExcerpt<'a> {
+	fn get_part(&'a self) -> &'a str {
+		self.part
+	}
+}
+
 fn longest<'a>(str0: &'a str, str1: &'a str) -> &'a str {
 	if str0.len() < str1.len() {
 		str1
@@ -19,5 +25,6 @@ fn main() {
 		part: first_sentence,
 	};
 	println!("Important excerpt = {:?}", important_excerpt);
+	println!("important_excerpt.get_part() = {}", important_excerpt.get_part());
 }
 
