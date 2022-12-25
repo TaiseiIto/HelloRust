8689bdb7ca43408613afd0191b6f3dba7cb34b82
TaiseiIto <taisei1212@outlook.jp>
2022-12-25 21:35:03 +0900

    12.3

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,17 +1,29 @@
 use std::io::prelude::*;
 
-fn main() {
-	let mut args: std::env::Args = std::env::args();
-	let program_name: String = args.next().expect("There is no program name.");
+struct Config {
+	query: String,
+	file_name: String,
+}
+
+fn parse_config(args: &mut std::env::Args) -> Config {
+	args.next().expect("There is no program name.");
 	let query: String = args.next().expect("There is no query.");
 	let file_name: String = args.next().expect("There is no file name.");
-	println!("program_name = \"{}\"", program_name);
-	println!("query = \"{}\"", query);
-	println!("file_name = \"{}\"", file_name);
-	let mut file: std::fs::File = std::fs::File::open(&file_name).expect(&format!("\"{}\" is not found.", &file_name));
+	Config {
+		query,
+		file_name
+	}
+}
+
+fn main() {
+	let config: Config = parse_config(&mut std::env::args());
+	println!("query = \"{}\"", config.query);
+	println!("file_name = \"{}\"", config.file_name);
+	let file_name: &str = &config.file_name;
+	let mut file: std::fs::File = std::fs::File::open(&file_name).expect(&format!("\"{}\" is not found.", file_name));
 	let mut contents: String = String::new();
-	file.read_to_string(&mut contents).expect(&format!("Something went wrong reading \"{}\"", &file_name));
-	println!("Contents of \"{}\"", &file_name);
+	file.read_to_string(&mut contents).expect(&format!("Something went wrong reading \"{}\"", file_name));
+	println!("Contents of \"{}\"", file_name);
 	println!("{}", contents);
 }
 
