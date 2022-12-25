2c2dffda5323d8440c395e944830008c6503cafa
TaiseiIto <taisei1212@outlook.jp>
2022-12-25 20:25:20 +0900

    11.3

---
+++ projects/book_11_3/tests/common/mod.rs
@@ -0,0 +1,4 @@
+pub fn setup() {
+	println!("setup is called!");
+}
+
--- projects/book_11_3/tests/integration_test.rs
+++ projects/book_11_3/tests/integration_test.rs
@@ -1,8 +1,11 @@
 extern crate book_11_3;
 use book_11_3::*;
+mod common;
+use common::*;
 
 #[test]
 fn it_adds_two() {
+	setup();
 	assert_eq!(add_two(2), 4);
 }
 
