50fc9116dbc9e054e7383f11ad11acd5140a9522
TaiseiIto <taisei1212@outlook.jp>
2022-12-26 17:06:21 +0900

    13.1

--- projects/book_13_1/src/main.rs
+++ projects/book_13_1/src/main.rs
@@ -1,41 +1,41 @@
 struct Cacher<T> where T: Fn(u32) -> u32 {
 	calculate: T,
-	value: Option<u32>,
+	cache: std::collections::HashMap<u32, u32>,
 }
 
 impl<T> Cacher<T> where T: Fn(u32) -> u32 {
 	fn new(calculate: T) -> Cacher<T> {
 		Cacher {
 			calculate,
-			value: None,
+			cache: std::collections::HashMap::new(),
 		}
 	}
 
-	fn value(&mut self, arg: u32) -> u32 {
-		match self.value {
-			Some(v) => v,
+	fn get(&mut self, arg: u32) -> u32 {
+		match self.cache.get(&arg) {
+			Some(result) => *result,
 			None => {
-				self.value = Some((self.calculate)(arg));
-				self.value(arg)
+				self.cache.insert(arg, (self.calculate)(arg));
+				self.get(arg)
 			}
 		}
 	}
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-	let mut expensive_calculator: Cacher<fn(u32) -> u32> = Cacher::new(|num: u32| -> u32 {
+	let mut expensive_cache: Cacher<fn(u32) -> u32> = Cacher::new(|num: u32| -> u32 {
 		println!("Calculating slowly ...");
 		std::thread::sleep(std::time::Duration::from_secs(2));
 		num
 	});
 	if intensity < 25 {
-		println!("Today, do {} pushups!", expensive_calculator.value(intensity));
-		println!("Next, do {} situps!", expensive_calculator.value(intensity));
+		println!("Today, do {} pushups!", expensive_cache.get(intensity));
+		println!("Next, do {} situps!", expensive_cache.get(intensity));
 	} else {
 		if random_number == 3 {
 			println!("Take a break today! Remember to stay hydrated!");
 		} else {
-			println!("Today, run for {} minutes!", expensive_calculator.value(intensity));
+			println!("Today, run for {} minutes!", expensive_cache.get(intensity));
 		}
 	}
 }
