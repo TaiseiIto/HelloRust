993cde22f8eca101e941b3293085695f8effc0e0
TaiseiIto <taisei1212@outlook.jp>
2022-12-26 15:20:11 +0900

    13.1

--- projects/book_13_1/src/main.rs
+++ projects/book_13_1/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+fn simulate_expensive_calculation(intensity: u32) -> u32 {
+	println!("Calculating slowly ...");
+	std::thread::sleep(std::time::Duration::from_secs(2));
+	intensity
 }
+
+fn generate_workout(intensity: u32, random_number: u32) {
+	if intensity < 25 {
+		println!("Today, do {} pushups!", simulate_expensive_calculation(intensity));
+		println!("Next, do {} situps!", simulate_expensive_calculation(intensity));
+	} else {
+		if random_number == 3 {
+			println!("Take a break today! Remember to stay hydrated!");
+		} else {
+			println!("Today, run for {} minutes!", simulate_expensive_calculation(intensity));
+		}
+	}
+}
+
+fn main() {
+	let user_specified_value: u32 = 10;
+	let random_number: u32 = 7;
+	generate_workout(user_specified_value, random_number);
+}
+
