d1fd6314a8f13a496ca49df82f064bddb87a16f3
TaiseiIto <taisei1212@outlook.jp>
2022-12-26 14:51:22 +0900

    12.6

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -2,11 +2,11 @@ extern crate minigrep;
 
 fn main() {
 	let config: minigrep::Config = minigrep::Config::new(&mut std::env::args()).unwrap_or_else(|error| {
-		println!("Error while parsing arguments: {}", error);
+		eprintln!("Error while parsing arguments: {}", error);
 		std::process::exit(1);
 	});
 	if let Err(error) = minigrep::run(&config) {
-		println!("Error while running: {}", error);
+		eprintln!("Error while running: {}", error);
 		std::process::exit(2);
 	}
 }
