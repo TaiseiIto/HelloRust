d85a011b7105669cc360673ee6c7c2fbd9e7e6d0
TaiseiIto <taisei1212@outlook.jp>
2022-12-26 19:48:12 +0900

    13.1

--- projects/book_13_1/src/main.rs
+++ projects/book_13_1/src/main.rs
@@ -1,21 +1,21 @@
-struct Cacher<T> where T: Fn(u32) -> u32 {
-	calculate: T,
-	cache: std::collections::HashMap<u32, u32>,
+struct Cacher<T, U> where T: Eq + std::hash::Hash {
+	calculate: fn(T) -> U,
+	cache: std::collections::HashMap<T, U>,
 }
 
-impl<T> Cacher<T> where T: Fn(u32) -> u32 {
-	fn new(calculate: T) -> Cacher<T> {
+impl<T, U> Cacher<T, U> where T: Eq + std::hash::Hash {
+	fn new(calculate: fn(T) -> U) -> Cacher<T, U> {
 		Cacher {
 			calculate,
 			cache: std::collections::HashMap::new(),
 		}
 	}
 
-	fn get(&mut self, arg: u32) -> u32 {
-		match self.cache.get(&arg) {
+	fn get(self: &mut Self, arg: &T) -> U where T: Copy, U: Copy {
+		match self.cache.get(arg) {
 			Some(result) => *result,
 			None => {
-				self.cache.insert(arg, (self.calculate)(arg));
+				self.cache.insert(*arg, (self.calculate)(*arg));
 				self.get(arg)
 			}
 		}
@@ -23,19 +23,19 @@ impl<T> Cacher<T> where T: Fn(u32) -> u32 {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-	let mut expensive_cache: Cacher<fn(u32) -> u32> = Cacher::new(|num: u32| -> u32 {
+	let mut expensive_cache: Cacher<u32, u32> = Cacher::new(|num: u32| -> u32 {
 		println!("Calculating slowly ...");
 		std::thread::sleep(std::time::Duration::from_secs(2));
 		num
 	});
 	if intensity < 25 {
-		println!("Today, do {} pushups!", expensive_cache.get(intensity));
-		println!("Next, do {} situps!", expensive_cache.get(intensity));
+		println!("Today, do {} pushups!", expensive_cache.get(&intensity));
+		println!("Next, do {} situps!", expensive_cache.get(&intensity));
 	} else {
 		if random_number == 3 {
 			println!("Take a break today! Remember to stay hydrated!");
 		} else {
-			println!("Today, run for {} minutes!", expensive_cache.get(intensity));
+			println!("Today, run for {} minutes!", expensive_cache.get(&intensity));
 		}
 	}
 }
