64b7a3c588d467cd5496273753eac98c84b39f93
TaiseiIto <taisei1212@outlook.jp>
2022-12-28 16:12:01 +0900

    13.2

--- projects/book_13_2/src/lib.rs
+++ projects/book_13_2/src/lib.rs
@@ -1,14 +1,18 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
+	#[test]
+	fn iterator_demonstration() {
+		let v = vec![1, 2, 3];
+		let mut iter = v.iter();
+		assert_eq!(iter.next(), Some(&1));
+		assert_eq!(iter.next(), Some(&2));
+		assert_eq!(iter.next(), Some(&3));
+		assert_eq!(iter.next(), None);
+	}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+	#[test]
+	fn iterator_sum() {
+		assert_eq!(vec![1, 2, 3].iter().sum::<i32>(), 6);
+	}
 }
+
