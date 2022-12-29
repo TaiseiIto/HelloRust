17dc9d0a8b87670dc296d8d4ffe45639d541a7e3
TaiseiIto <taisei1212@outlook.jp>
2022-12-29 14:48:40 +0900

    14.2

--- projects/book_14_2/src/lib.rs
+++ projects/book_14_2/src/lib.rs
@@ -13,6 +13,59 @@ pub fn add_one(x: i32) -> i32 {
 	x + 1
 }
 
+/// # Art
+///
+/// A library for modeling artistic concepts.
+pub mod kinds {
+	/// The primary colors according to the RYB color model.
+	pub enum PrimaryColor {
+		Red,
+		Yellow,
+		Blue,
+	}
+
+	/// The secondary colors according to the RYB color model.
+	pub enum SecondaryColor {
+		Primary {
+			primary: PrimaryColor,
+		},
+		Orange,
+		Green,
+		Purple,
+	}
+}
+
+pub mod utils {
+	use super::kinds::*;
+
+	/// It combines two primary colors in equal amounts to create a secondary color.
+	pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+		match c1 {
+			PrimaryColor::Red => match c2 {
+				PrimaryColor::Red => SecondaryColor::Primary {
+					primary: PrimaryColor::Red
+				},
+				PrimaryColor::Yellow => SecondaryColor::Orange,
+				PrimaryColor::Blue => SecondaryColor::Purple,
+			},
+			PrimaryColor::Yellow => match c2 {
+				PrimaryColor::Red => SecondaryColor::Orange,
+				PrimaryColor::Yellow => SecondaryColor::Primary {
+					primary: PrimaryColor::Yellow
+				},
+				PrimaryColor::Blue => SecondaryColor::Green,
+			},
+			PrimaryColor::Blue => match c2 {
+				PrimaryColor::Red => SecondaryColor::Purple,
+				PrimaryColor::Yellow => SecondaryColor::Green,
+				PrimaryColor::Blue => SecondaryColor::Primary {
+					primary: PrimaryColor::Blue
+				},
+			},
+		}
+	}
+}
+
 #[cfg(test)]
 mod test {
 	#[test]
