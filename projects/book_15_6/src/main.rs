a95262fe1d00de43227766961cce00ca8e01f06e
TaiseiIto <taisei1212@outlook.jp>
2022-12-30 20:23:24 +0900

    15.6

--- projects/book_15_6/src/main.rs
+++ projects/book_15_6/src/main.rs
@@ -1,3 +1,18 @@
+#[derive(Debug)]
+enum List<T> {
+	Cons(T, std::cell::RefCell<std::rc::Rc<List<T>>>),
+	Nil,
+}
+
+impl<T> List<T> {
+	fn tail(self: &Self) -> Option<&std::cell::RefCell<std::rc::Rc<List<T>>>> {
+		match *self {
+			Self::Cons(_, ref item) => Some(item),
+			Self::Nil => None,
+		}
+	}
+}
+
 fn main() {
     println!("Hello, world!");
 }
