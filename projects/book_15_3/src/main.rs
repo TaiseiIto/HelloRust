780cc8dc463ac18ca066aee914b813c2b099ab76
TaiseiIto <taisei1212@outlook.jp>
2022-12-30 14:36:50 +0900

    15.3

--- projects/book_15_3/src/main.rs
+++ projects/book_15_3/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+struct CustomSmartPointer {
+	data: String,
 }
+
+impl Drop for CustomSmartPointer {
+	fn drop(&mut self) {
+		println!("Dropping CustomSmartPointer with data {}", self.data);
+	}
+}
+
+fn main() {
+	let c = CustomSmartPointer {
+		data: "my stuff".to_string(),
+	};
+	let _d = CustomSmartPointer {
+		data: "other stuff".to_string(),
+	};
+	drop(c);
+	println!("CustomSmartPointers created.");
+}
+
