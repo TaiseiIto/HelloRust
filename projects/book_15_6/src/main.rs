6f15886a8a347250e18bc553aa644e339d67aa68
TaiseiIto <taisei1212@outlook.jp>
2022-12-30 20:40:21 +0900

    15.6

--- projects/book_15_6/src/main.rs
+++ projects/book_15_6/src/main.rs
@@ -5,6 +5,18 @@ enum List<T> {
 }
 
 impl<T> List<T> {
+	fn cons(list: std::cell::RefCell<std::rc::Rc<List<T>>>, x: T) -> std::cell::RefCell<std::rc::Rc<List<T>>> {
+		std::cell::RefCell::new(std::rc::Rc::new(Self::Cons(x, list)))
+	}
+
+	fn new(v: Vec<T>) -> std::cell::RefCell<std::rc::Rc<List<T>>> {
+		let mut list: std::cell::RefCell<std::rc::Rc<List<T>>> = std::cell::RefCell::new(std::rc::Rc::new(Self::Nil));
+		for x in v.into_iter().rev() {
+			list = Self::cons(list, x);
+		}
+		list
+	}
+
 	fn tail(self: &Self) -> Option<&std::cell::RefCell<std::rc::Rc<List<T>>>> {
 		match *self {
 			Self::Cons(_, ref item) => Some(item),
@@ -14,5 +26,7 @@ impl<T> List<T> {
 }
 
 fn main() {
-    println!("Hello, world!");
+	let a: std::cell::RefCell<std::rc::Rc<List<i32>>> = List::new(vec![5]);
+	println!("a = {:#?}", a);
 }
+
