06b36a29246ced03eea1e9c05b101bd6a2784155
TaiseiIto <taisei1212@outlook.jp>
2022-12-30 15:06:53 +0900

    15.4

--- projects/book_15_4/src/main.rs
+++ projects/book_15_4/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+enum List<T> {
+	Cons(T, std::rc::Rc<List<T>>),
+	Nil,
 }
+
+impl<T> List<T> {
+	fn cons(self: Self, x: T) -> List<T> {
+		List::Cons(x, std::rc::Rc::new(self))
+	}
+
+	fn new(v: Vec<T>) -> List<T> {
+		let mut list: List<T> = List::Nil;
+		for i in v.into_iter().rev() {
+			list = list.cons(i);
+		}
+		list
+	}
+}
+
+fn main() {
+	let a: List<i32> = List::new(vec![5, 10]);
+	println!("a = {:#?}", a);
+}
+
