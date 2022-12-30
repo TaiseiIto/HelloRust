fec2978aae3fe425edf8cef806f9b4eea06b5e3f
TaiseiIto <taisei1212@outlook.jp>
2022-12-30 13:08:09 +0900

    15.1

--- projects/book_15_1/src/main.rs
+++ projects/book_15_1/src/main.rs
@@ -1,18 +1,18 @@
 #[derive(Debug)]
-enum List<T> where T: Clone {
+enum List<T> {
 	Cons(T, Box<List<T>>),
 	Nil,
 }
 
-impl<T> List<T> where T: Clone {
+impl<T> List<T> {
 	fn cons(self, x: T) -> List<T> {
 		List::Cons(x, Box::new(self))
 	}
 
 	fn new(v: Vec<T>) -> List<T> {
 		let mut list: List<T> = List::Nil;
-		for x in v.iter().rev() {
-			list = list.cons(x.clone());
+		for x in v.into_iter().rev() {
+			list = list.cons(x);
 		}
 		list
 	}
