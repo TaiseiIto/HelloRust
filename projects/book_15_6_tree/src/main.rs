bd4df2ce42c01bab78db8bbd22fc82452e9078e7
TaiseiIto <taisei1212@outlook.jp>
2022-12-30 21:56:32 +0900

    15.6

--- projects/book_15_6_tree/src/main.rs
+++ projects/book_15_6_tree/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct Node<T> {
+	value: T,
+	parent: std::cell::RefCell<std::rc::Weak<Node<T>>>,
+	children: std::cell::RefCell<Vec<std::rc::Rc<Node<T>>>>,
 }
+
+impl<T> Node<T> {
+	fn new_leaf(x: T) -> std::rc::Rc<Self> {
+		std::rc::Rc::new(Self {
+			value: x,
+			parent: std::cell::RefCell::new(std::rc::Weak::new()),
+			children: std::cell::RefCell::new(vec![]),
+		})
+	}
+}
+
+fn main() {
+	let leaf: std::rc::Rc<Node<i32>> = Node::new_leaf(3);
+	let branch: std::rc::Rc<Node<i32>> = std::rc::Rc::new(Node {
+		value: 5,
+		parent: std::cell::RefCell::new(std::rc::Weak::new()),
+		children: std::cell::RefCell::new(vec![std::rc::Rc::clone(&leaf)]),
+	});
+	println!("leaf = {:#?}", leaf);
+	println!("branch = {:#?}", branch);
+}
+
