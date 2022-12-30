aa5d4737cb769fb73252f33e9cc6e07826a5e7c5
TaiseiIto <taisei1212@outlook.jp>
2022-12-30 13:48:07 +0900

    15.2

--- projects/book_15_2/src/main.rs
+++ projects/book_15_2/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+	fn new(x: T) -> MyBox<T> {
+		MyBox(x)
+	}
 }
+
+impl<T> std::ops::Deref for MyBox<T> {
+	type Target = T;
+
+	fn deref(&self) -> &T {
+		&self.0
+	}
+}
+
+fn main() {
+	let x = 5;
+	let y = MyBox::new(x);
+	println!("x = {}", x);
+	println!("*y = {}", *y);
+}
+
