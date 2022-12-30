053023145ba3375a0ceb1f1d83ea6317427b5ff4
TaiseiIto <taisei1212@outlook.jp>
2022-12-30 21:30:09 +0900

    15.6

--- projects/book_15_6/src/main.rs
+++ projects/book_15_6/src/main.rs
@@ -1,27 +1,46 @@
 #[derive(Debug)]
+struct VisibleDrop<T> where T: std::fmt::Debug {
+	x: T,
+}
+
+impl<T> VisibleDrop<T> where T: std::fmt::Debug {
+	fn new(x: T) -> Self {
+		VisibleDrop {
+			x,
+		}
+	}
+}
+
+impl<T> Drop for VisibleDrop<T> where T: std::fmt::Debug {
+	fn drop(self: &mut Self) {
+		println!("Drop {:#?}!!!", self.x);
+	}
+}
+
+#[derive(Debug)]
 enum List<T> {
 	Cons(T, std::cell::RefCell<std::rc::Rc<List<T>>>),
 	Nil,
 }
 
 impl<T> List<T> {
-	fn cons(list: std::cell::RefCell<std::rc::Rc<List<T>>>, x: T) -> std::cell::RefCell<std::rc::Rc<List<T>>> {
+	fn cons(list: std::cell::RefCell<std::rc::Rc<Self>>, x: T) -> std::cell::RefCell<std::rc::Rc<Self>> {
 		std::cell::RefCell::new(std::rc::Rc::new(Self::Cons(x, list)))
 	}
 
-	fn cons_branch(list: &std::cell::RefCell<std::rc::Rc<List<T>>>, x: T) -> std::cell::RefCell<std::rc::Rc<List<T>>> {
+	fn cons_branch(list: &std::cell::RefCell<std::rc::Rc<Self>>, x: T) -> std::cell::RefCell<std::rc::Rc<Self>> {
 		std::cell::RefCell::new(std::rc::Rc::new(Self::Cons(x, std::cell::RefCell::new(std::rc::Rc::clone(&list.borrow())))))
 	}
 
-	fn new(v: Vec<T>) -> std::cell::RefCell<std::rc::Rc<List<T>>> {
-		let mut list: std::cell::RefCell<std::rc::Rc<List<T>>> = std::cell::RefCell::new(std::rc::Rc::new(Self::Nil));
+	fn new(v: Vec<T>) -> std::cell::RefCell<std::rc::Rc<Self>> {
+		let mut list: std::cell::RefCell<std::rc::Rc<Self>> = std::cell::RefCell::new(std::rc::Rc::new(Self::Nil));
 		for x in v.into_iter().rev() {
 			list = Self::cons(list, x);
 		}
 		list
 	}
 
-	fn tail(self: &Self) -> Option<&std::cell::RefCell<std::rc::Rc<List<T>>>> {
+	fn tail(self: &Self) -> Option<&std::cell::RefCell<std::rc::Rc<Self>>> {
 		match *self {
 			Self::Cons(_, ref item) => Some(item),
 			Self::Nil => None,
@@ -30,10 +49,10 @@ impl<T> List<T> {
 }
 
 fn main() {
-	let a: std::cell::RefCell<std::rc::Rc<List<i32>>> = List::new(vec![5]);
+	let a: std::cell::RefCell<std::rc::Rc<List<VisibleDrop<i32>>>> = List::new(vec![VisibleDrop::new(5)]);
 	println!("a = {:#?}", a);
 	println!("a rc count = {}", std::rc::Rc::strong_count(&a.borrow()));
-	let b: std::cell::RefCell<std::rc::Rc<List<i32>>> = List::cons_branch(&a, 10);
+	let b: std::cell::RefCell<std::rc::Rc<List<VisibleDrop<i32>>>> = List::cons_branch(&a, VisibleDrop::new(10));
 	println!("b = {:#?}", b);
 	println!("a rc count = {}", std::rc::Rc::strong_count(&a.borrow()));
 	println!("b rc count = {}", std::rc::Rc::strong_count(&b.borrow()));
