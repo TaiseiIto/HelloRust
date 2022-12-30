41e9dd1ad8888b6d203e54156609418c09ed2a71
TaiseiIto <taisei1212@outlook.jp>
2022-12-30 13:06:51 +0900

    15.1

--- projects/book_15_1/src/main.rs
+++ projects/book_15_1/src/main.rs
@@ -1,19 +1,27 @@
 #[derive(Debug)]
-enum List<T> {
+enum List<T> where T: Clone {
 	Cons(T, Box<List<T>>),
 	Nil,
 }
 
-impl<T> List<T> {
-	fn new(_: &Vec<T>) -> List<T> {
-		List::Nil
+impl<T> List<T> where T: Clone {
+	fn cons(self, x: T) -> List<T> {
+		List::Cons(x, Box::new(self))
+	}
+
+	fn new(v: Vec<T>) -> List<T> {
+		let mut list: List<T> = List::Nil;
+		for x in v.iter().rev() {
+			list = list.cons(x.clone());
+		}
+		list
 	}
 }
 
 fn main() {
 	let b: Box<i32> = Box::new(5);
     println!("b = {}", b);
-	let list: List<i32> = List::new(&vec![1,2,3]);
+	let list: List<i32> = List::new(vec![1,2,3]);
 	println!("list = {:#?}", list);
 }
 
