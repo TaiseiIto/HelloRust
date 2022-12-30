8edaba33d3fec895bf4bd074d11a843c34ab637a
TaiseiIto <taisei1212@outlook.jp>
2022-12-30 10:51:41 +0900

    15.1

--- projects/book_15_1/src/main.rs
+++ projects/book_15_1/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+enum List<T> {
+	Cons(T, Box<List<T>>),
+	Nil,
 }
+
+impl<T> List<T> {
+	fn new(_: &Vec<T>) -> List<T> {
+		List::Nil
+	}
+}
+
+fn main() {
+	let b: Box<i32> = Box::new(5);
+    println!("b = {}", b);
+	let list: List<i32> = List::new(&vec![1,2,3]);
+	println!("list = {:#?}", list);
+}
+
