471351d7b5da54bde54c8ce038c1550c239380dc
TaiseiIto <taisei1212@outlook.jp>
2022-12-30 16:32:40 +0900

    15.5

--- projects/book_15_5/src/lib.rs
+++ projects/book_15_5/src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub trait Messenger {
+	fn send(&self, msg: &str);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub struct LimitTracker<'a, T: 'a + Messenger> {
+	messenger: &'a T,
+	value: usize,
+	max: usize,
 }
+
+impl<'a, T> LimitTracker<'a, T> where T: Messenger {
+	pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+		LimitTracker {
+			messenger,
+			value: 0,
+			max,
+		}
+	}
+
+	pub fn set_value(&mut self, value: usize) {
+		self.value = value;
+		let percentage_of_max: f64 = self.value as f64 / self.max as f64;
+		if 0.75 <= percentage_of_max && percentage_of_max < 0.9 {
+			self.messenger.send("Warning: You've used up over 75% of your quota!");
+		} else if 0.9 <= percentage_of_max && percentage_of_max < 1.0 {
+			self.messenger.send("Urgent warning: You've used up over 90% of your quota!");
+		} else if 1.0 <= percentage_of_max {
+			self.messenger.send("Error: You are over your quota!");
+		}
+	}
+}
+
