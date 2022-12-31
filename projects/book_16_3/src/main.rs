b08bf2970d758a7c37a0eb027e77b54a7041de6b
TaiseiIto <taisei1212@outlook.jp>
2022-12-31 16:41:56 +0900

    16.3

--- projects/book_16_3/src/main.rs
+++ projects/book_16_3/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+	let mutex: std::sync::Mutex<i32> = std::sync::Mutex::new(5);
+	{
+		let mut num: std::sync::MutexGuard<i32> = mutex.lock().unwrap();
+		*num = 6;
+	}
+	let num: std::sync::MutexGuard<i32> = mutex.lock().unwrap();
+	println!("*num = {:#?}", *num);
+	let counter: std::sync::Arc<std::sync::Mutex<i32>> = std::sync::Arc::new(std::sync::Mutex::new(0));
+	let handles: Vec<std::thread::JoinHandle<()>> = (0..10).map(|_| {
+		let counter: std::sync::Arc<std::sync::Mutex<i32>> = std::sync::Arc::clone(&counter);
+		std::thread::spawn(move || {
+			let mut num = counter.lock().unwrap();
+			*num += 1;
+		})
+	}).collect::<Vec<std::thread::JoinHandle<()>>>();
+	for handle in handles {
+		handle.join().unwrap();
+	}
+	println!("Result: {}", *counter.lock().unwrap());
 }
+
