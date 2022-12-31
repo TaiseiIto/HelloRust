67c2d9e979da33f44efed344f58ac7fb0ec586bd
TaiseiIto <taisei1212@outlook.jp>
2022-12-31 09:41:42 +0900

    16.1

--- projects/book_16_1/src/main.rs
+++ projects/book_16_1/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+	std::thread::spawn(|| {
+		for i in 1..10 {
+			println!("Hi! number {} from the spawned thread!", i);
+			std::thread::sleep(std::time::Duration::from_millis(1));
+		}
+	});
+	for i in 1..10 {
+			println!("Hi! number {} from the main thread!", i);
+			std::thread::sleep(std::time::Duration::from_millis(1));
+	}
 }
+
