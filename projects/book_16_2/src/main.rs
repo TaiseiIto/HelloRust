7bfb98122eaf49d502d994228c1098ee59fb0f0a
TaiseiIto <taisei1212@outlook.jp>
2022-12-31 15:01:50 +0900

    16.2

--- projects/book_16_2/src/main.rs
+++ projects/book_16_2/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+	let (tx, rx) = std::sync::mpsc::channel();
+	let thread: std::thread::JoinHandle<()> = std::thread::spawn(move || {
+		let val: String = "hi".to_string();
+		tx.send(val).unwrap();
+	});
+	let received: String = rx.recv().unwrap();
+	println!("Got: {}", received);
+	thread.join().unwrap();
 }
+
