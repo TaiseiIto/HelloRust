76b2c150f320fbd4539db757ff7ee38f33ece2f8
TaiseiIto <taisei1212@outlook.jp>
2022-12-31 22:40:53 +0900

    17.3

--- projects/book_17_3/src/lib.rs
+++ projects/book_17_3/src/lib.rs
@@ -1,10 +1,10 @@
 trait State {
-    fn request_review(self: Box<Self>) -> Box<Self>;
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
 
-    fn approve(self: Box<Self>) -> Box<Self>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
 
     fn content<'a>(&self, post: &'a Post) -> &'a str {
-        post.content
+        &post.content
     }
 }
 
@@ -19,11 +19,11 @@ impl Published {
 }
 
 impl State for Published {
-    fn request_review(self: Box<Self>) -> Box<Self> {
+    fn request_review(self: Box<Published>) -> Box<dyn State> {
         self
     }
 
-    fn approve(self: Box<Self>) -> Box<Self> {
+    fn approve(self: Box<Published>) -> Box<dyn State> {
         self
     }
 }
@@ -39,11 +39,11 @@ impl PendingReview {
 }
 
 impl State for PendingReview {
-    fn request_review(self: Box<Self>) -> Box<Self> {
+    fn request_review(self: Box<PendingReview>) -> Box<dyn State> {
         self
     }
 
-    fn approve(self: Box<Self>) -> Box<Self> {
+    fn approve(self: Box<PendingReview>) -> Box<dyn State> {
         Box::new(Published::new())
     }
 }
@@ -52,17 +52,17 @@ struct Draft {
 }
 
 impl State for Draft {
-    fn request_review(self: Box<Self>) -> Box<Self> {
+    fn request_review(self: Box<Draft>) -> Box<dyn State> {
         Box::new(PendingReview::new())
     }
 
-    fn approve(self: Box<Self>) -> Box<Self> {
+    fn approve(self: Box<Draft>) -> Box<dyn State> {
         self
     }
 }
 
 pub struct Post {
-    state: Option<Box<State>>,
+    state: Option<Box<dyn State>>,
     content: String,
 }
 
@@ -81,7 +81,9 @@ impl Post {
     pub fn content(&self) -> &str {
         if let Some(state) = self.state.as_ref() {
             state.content(self)
-        } else "None!!!"
+        } else {
+			"None!!!"
+		}
     }
 
     pub fn request_review(&mut self) {
