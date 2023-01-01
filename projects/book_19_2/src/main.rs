dc6eab803530554710d8e5a1c65e3d22874a2d21
TaiseiIto <taisei1212@outlook.jp>
2023-01-01 20:10:04 +0900

    19.2

--- projects/book_19_2/src/main.rs
+++ projects/book_19_2/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+	let p = Point::new(1, 2);
+	let q = Point::new(3, 4);
+	let r = p + q;
+	println!("r = {:?}", r);
 }
+
+#[derive(Debug)]
+struct Point {
+	x: i32,
+	y: i32,
+}
+
+impl Point {
+	fn new(x: i32, y: i32) -> Self {
+		Self {x, y}
+	}
+}
+
+impl std::ops::Add for Point {
+	type Output = Self;
+
+	fn add(self, other: Self) -> Self {
+		Self {
+			x: self.x + other.x,
+			y: self.y + other.y,
+		}
+	}
+}
+
