3228f897116a25cc5af4da29a3fb70f356c2fb06
TaiseiIto <taisei1212@outlook.jp>
2023-01-01 14:52:40 +0900

    18.3

--- projects/book_18_3/src/main.rs
+++ projects/book_18_3/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+	println!("match_literal(0) = {}", match_literal(0));
+	println!("match_literal(1) = {}", match_literal(1));
+	println!("match_literal(2) = {}", match_literal(2));
+	println!("match_literal(3) = {}", match_literal(3));
 }
+
+fn match_literal(x: u32) -> &'static str {
+	match x {
+		0 => "zero",
+		1 => "one",
+		2 => "two",
+		_ => "many",
+	}
+}
+
