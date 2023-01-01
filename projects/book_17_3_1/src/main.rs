adfdf9d7fa3ea6eca1cd13b9a3a1ae5ef3f25216
TaiseiIto <taisei1212@outlook.jp>
2023-01-01 13:11:00 +0900

    17.3

--- projects/book_17_3_1/src/main.rs
+++ projects/book_17_3_1/src/main.rs
@@ -1,3 +1,12 @@
+extern crate book_17_3_1;
+
+use book_17_3_1::*;
+
 fn main() {
-    println!("Hello, world!");
+	let mut post = Post::new();
+	post.add_text("I ate a salad for lunch today.");
+    let post = post.request_review();
+    let post = post.approve();
+	println!("post.content() = \"{}\"", post.content());
 }
+
