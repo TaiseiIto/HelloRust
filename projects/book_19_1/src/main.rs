5b98e8e1ae6e6efde08b45be44c92918f881ffea
TaiseiIto <taisei1212@outlook.jp>
2023-01-01 19:23:30 +0900

    19.1

--- projects/book_19_1/src/main.rs
+++ projects/book_19_1/src/main.rs
@@ -4,7 +4,24 @@ fn main() {
 	let mutable_pointer = &mut num as *mut i32;
 	unsafe {
 		println!("num = {}", *immutable_pointer);
-		println!("num = {}", *mutable_pointer);
+		*mutable_pointer += 1;
+		println!("num = {}", *immutable_pointer);
+	}
+	let mut v = [1, 2, 3, 4, 5, 6];
+	let (a, b) = split_at_mut(&mut v[..], 3);
+	println!("a = {:?}", a);
+	println!("b = {:?}", b);
+}
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+	let len: usize = slice.len();
+	let ptr: *mut i32 = slice.as_mut_ptr();
+	assert!(mid <= len);
+	unsafe {
+		(
+			std::slice::from_raw_parts_mut(ptr, mid),
+			std::slice::from_raw_parts_mut(ptr.offset(mid as isize), len - mid),
+		)
 	}
 }
 
