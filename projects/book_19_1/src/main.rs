7210571ca70d6595b9f7aa51e03e820b541b4402
TaiseiIto <taisei1212@outlook.jp>
2023-01-01 18:57:16 +0900

    19.1

--- projects/book_19_1/src/main.rs
+++ projects/book_19_1/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+	let mut num: i32 = 5;
+	let immutable_pointer = &num as *const i32;
+	let mutable_pointer = &mut num as *mut i32;
+	unsafe {
+		println!("num = {}", *immutable_pointer);
+		println!("num = {}", *mutable_pointer);
+	}
 }
+
