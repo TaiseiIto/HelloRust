517826363cbc9cfa57a2289c9591ceb240a8ac0f
TaiseiIto <taisei1212@outlook.jp>
2023-01-01 14:47:38 +0900

    18.1

--- projects/book_18_1/src/main.rs
+++ projects/book_18_1/src/main.rs
@@ -3,5 +3,18 @@ fn main() {
 	while let Some(x) = stack.pop() {
 		println!("x = {}", x);
 	}
+	let vector: Vec<char> = vec!['a', 'b', 'c'];
+	for (index, chr) in vector.iter().enumerate() {
+		println!("vector[{}] = {}", index, chr);
+	}
+	let (x, y, ..) = (1, 2, 3, 4, 5);
+	println!("x = {}", x);
+	println!("y = {}", y);
+	let point: (i32, i32) = (3, 5);
+	print_coordinates(point);
+}
+
+fn print_coordinates((x, y): (i32, i32)) {
+	println!("Current location: ({}, {})", x, y);
 }
 
