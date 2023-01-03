fb671f1e877109f8489023407c719a9f3c6a09db
TaiseiIto <taisei1212@outlook.jp>
2023-01-03 14:39:33 +0900

    20.1

--- projects/hello/src/main.rs
+++ projects/hello/src/main.rs
@@ -17,22 +17,17 @@ fn handle_connection(mut stream: std::net::TcpStream) {
 	let request: String = (*String::from_utf8_lossy(&buffer[..])).to_string();
 	println!("request = {}", request);
 	let get: &str = "GET / HTTP/1.1\r\n";
-	if request.starts_with(get) {
-		let mut html_file: std::fs::File = std::fs::File::open("hello.html").unwrap();
-		let mut html: String = String::new();
-		html_file.read_to_string(&mut html).unwrap();
-		let responce: String = format!("HTTP/1.1 200 OK\n\n{}", html);
-		let responce: String = responce.replace("\n", "\r\n");
-		stream.write(responce.as_bytes()).unwrap();
-		stream.flush().unwrap();
+	let (header, html_file_name) = if request.starts_with(get) {
+		("HTTP/1.1 200 OK", "hello.html")
 	} else {
-		let mut html_file: std::fs::File = std::fs::File::open("404.html").unwrap();
-		let mut html: String = String::new();
-		html_file.read_to_string(&mut html).unwrap();
-		let responce: String = format!("HTTP/1.1 404 NOT FOUND\n\n{}", html);
-		let responce: String = responce.replace("\n", "\r\n");
-		stream.write(responce.as_bytes()).unwrap();
-		stream.flush().unwrap();
-	}
+		("HTTP/1.1 404 NOT FOUND", "404.html")
+	};
+	let mut html_file: std::fs::File = std::fs::File::open(html_file_name).unwrap();
+	let mut html: String = String::new();
+	html_file.read_to_string(&mut html).unwrap();
+	let responce: String = format!("{}\n\n{}", header, html);
+	let responce: String = responce.replace("\n", "\r\n");
+	stream.write(responce.as_bytes()).unwrap();
+	stream.flush().unwrap();
 }
 
