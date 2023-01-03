e58ea0700ad4463bb407d6bdf5a96afef4eeac6f
TaiseiIto <taisei1212@outlook.jp>
2023-01-03 11:37:42 +0900

    20.1

--- projects/hello/src/main.rs
+++ projects/hello/src/main.rs
@@ -1,10 +1,18 @@
+use std::io::Read;
 use std::net::TcpListener;
 
 fn main() {
 	let listener: std::net::TcpListener = TcpListener::bind("127.0.0.1:7878").unwrap();
 	for stream in listener.incoming() {
 		let stream = stream.unwrap();
-		println!("Connection established.");
+		handle_connection(stream);
 	}
+}
+
+fn handle_connection(mut stream: std::net::TcpStream) {
+	let mut buffer: [u8; 1024] = [0; 1024];
+	stream.read(&mut buffer).unwrap();
+	let request: String = (*String::from_utf8_lossy(&buffer[..])).to_string();
+	println!("request = {}", request);
 }
 
