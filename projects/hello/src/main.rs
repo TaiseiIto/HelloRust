c7947523365d81069f2be79d28836014c38756f2
TaiseiIto <taisei1212@outlook.jp>
2023-01-03 12:07:37 +0900

    20.1

--- projects/hello/src/main.rs
+++ projects/hello/src/main.rs
@@ -3,10 +3,8 @@ use std::net::TcpListener;
 
 fn main() {
 	let listener: std::net::TcpListener = TcpListener::bind("127.0.0.1:7878").unwrap();
-	for stream in listener.incoming() {
-		let stream = stream.unwrap();
-		handle_connection(stream);
-	}
+	let stream = listener.incoming().next().unwrap().unwrap();
+	handle_connection(stream);
 }
 
 fn handle_connection(mut stream: std::net::TcpStream) {
