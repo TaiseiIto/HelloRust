fa8e149b8ca3391aa7b8c2a111923ae47b21cd8d
TaiseiIto <taisei1212@outlook.jp>
2023-01-03 14:16:10 +0900

    20.1

--- projects/hello/src/main.rs
+++ projects/hello/src/main.rs
@@ -1,4 +1,5 @@
 use std::io::Read;
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
@@ -15,5 +16,9 @@ fn handle_connection(mut stream: std::net::TcpStream) {
 	stream.read(&mut buffer).unwrap();
 	let request: String = (*String::from_utf8_lossy(&buffer[..])).to_string();
 	println!("request = {}", request);
+	let responce: &str = "HTTP/1.1 200 OK\n\n";
+	let responce: &str = &responce.replace("\n", "\r\n");
+	stream.write(responce.as_bytes()).unwrap();
+	stream.flush().unwrap();
 }
 
