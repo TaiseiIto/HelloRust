bf0a317264432c4a08edbaeaf0a8ea3a38e08234
TaiseiIto <taisei1212@outlook.jp>
2023-01-03 12:04:45 +0900

    20.1

--- projects/hello/client.sh
+++ projects/hello/client.sh
@@ -13,6 +13,6 @@ sleep 1
 curl localhost:$port
 sleep 1
 if [ $(ps -a | grep $project | wc -l) -eq 1 ] ; then
-	kill $(ps -a | preg $project | awk '{print \$1}')
+	kill $(ps -a | grep $project | awk '{print $1}')
 fi
 
