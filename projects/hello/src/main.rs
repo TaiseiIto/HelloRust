7992431eb4231136c6b269f300d6147886694988
TaiseiIto <taisei1212@outlook.jp>
2023-01-03 14:23:44 +0900

    20.1

--- projects/hello/src/main.rs
+++ projects/hello/src/main.rs
@@ -16,8 +16,11 @@ fn handle_connection(mut stream: std::net::TcpStream) {
 	stream.read(&mut buffer).unwrap();
 	let request: String = (*String::from_utf8_lossy(&buffer[..])).to_string();
 	println!("request = {}", request);
-	let responce: &str = "HTTP/1.1 200 OK\n\n";
-	let responce: &str = &responce.replace("\n", "\r\n");
+	let html_file: std::fs::File = std::fn::File::open("hello.html").unwrap();
+	let mut html: String = String::new();
+	html_file.read_to_string(&mut html).unwrap();
+	let responce: String = format!("HTTP/1.1 200 OK\n\n{}", html);
+	let responce: String = responce.replace("\n", "\r\n");
 	stream.write(responce.as_bytes()).unwrap();
 	stream.flush().unwrap();
 }
