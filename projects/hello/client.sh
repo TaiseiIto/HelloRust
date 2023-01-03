6172f48441bbcc6312900d5f859f40c8ffe71dd1
TaiseiIto <taisei1212@outlook.jp>
2023-01-03 12:03:17 +0900

    20.1

--- projects/hello/client.sh
+++ projects/hello/client.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-if [ $# -ne 1 ] ; then
+if [ $# -ne 2 ] ; then
 	echo "Usage: ./client.sh <project name>"
 	exit
 fi
 project=$1
+port=$2
 while [ $(ps -a | grep $project | wc -l) -eq 0 ] ; do
 	sleep 1
 done
+sleep 1
+curl localhost:$port
+sleep 1
+if [ $(ps -a | grep $project | wc -l) -eq 1 ] ; then
+	kill $(ps -a | preg $project | awk '{print \$1}')
+fi
 
